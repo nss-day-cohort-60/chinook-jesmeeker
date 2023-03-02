ee6396788967987eebc7aa72b933a6c5b1fe4952
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-02 10:06:45 -0600

    top_2009_agent.sql actually working right

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,7 +1,13 @@
 SELECT e.FirstName || ' ' || e.LastName as TopSalesAgent2009,
-    SUM(i.Total) as TotalSales
-From Employee as e
-    JOIN Customer as c ON c.SupportRepId = e.EmployeeId
-    Join Invoice as i on i.CustomerId = c.CustomerId
-WHERE i.InvoiceDate LIKE "2009%"
-ORDER BY MAX(i.Total);
+    MAX(totalSales)
+from (
+        SELECT e.employeeId as ID,
+            e.FirstName || ' ' || e.LastName as TopSalesAgent2009,
+            SUM(i.Total) as totalSales
+        From Employee as e
+            JOIN Customer as c ON c.SupportRepId = e.EmployeeId
+            Join Invoice as i on i.CustomerId = c.CustomerId
+        WHERE i.InvoiceDate LIKE "2009%"
+        GROUP BY TopSalesAgent2009
+    )
+    INNER JOIN Employee as e on e.employeeId = ID;
